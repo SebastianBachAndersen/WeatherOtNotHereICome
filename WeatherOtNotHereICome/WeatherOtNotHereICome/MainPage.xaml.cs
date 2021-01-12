1d1d1a8d5c17900dcb1667d7e155f2177ef43af8
Seppi hr. Chef <SHDa.skp@edu.mercantec.dk>
2021-01-12 16:40:09 +0100

    Functioning UI with dummy data

--- WeatherOtNotHereICome/WeatherOtNotHereICome/MainPage.xaml.cs
+++ WeatherOtNotHereICome/WeatherOtNotHereICome/MainPage.xaml.cs
@@ -16,6 +16,33 @@ namespace WeatherOtNotHereICome
         public MainPage()
         {
             InitializeComponent();
+            SetItemSource(null, null);
+        }
+        public void SetItemSource(object sender, EventArgs e)
+        {
+            List<DummyClass> yeet = new List<DummyClass>();
+            for (int i = 0; i < 10; i++)
+            {
+                yeet.Add(new DummyClass("10:00", "oi", "yeed", "AAAAAA"));
+            }
+            carouselView.ItemsSource = yeet;
+        }
+    }
+
+    public class DummyClass
+    {
+        public string Time { get; set; }
+        public string Temp { get; set; }
+        public string Humidity { get; set; }
+        public string Uv { get; set; }
+
+
+        public DummyClass(string time, string temp, string humidity, string uv)
+        {
+            Time = time;
+            Temp = temp;
+            Humidity = humidity;
+            Uv = uv;
         }
     }
 }
